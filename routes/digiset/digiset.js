6043f5c14184a62c86eedd7d1140afb7ddcdddc6
iknowlation <dewadkar@gmail.com>
2019-08-13 18:57:44 +0530

    added digiset details page

--- routes/digiset/digiset.js
+++ routes/digiset/digiset.js
@@ -17,6 +17,21 @@ module.exports = function (app) {
         response.render('digiset/digiset', { title: "Digiset" });
     };
 
+    /**
+     * @function digisetDetailsView
+     * @description Render the Digiset Details page 
+     * @param request: HTTP Request
+     * @param response: HTTP Response
+     * @returns Render ejs page
+     */
+    this.digisetDetailsView = function (request, response) {
+        // var loc = location + 'digisetDetailsView';
+        response.render('digiset/digiset_details', { title: "Digiset Details", generator_id: request.params.id });
+    };
+
     // Digiset Page
     app.get('/digiset', this.view);
+
+    // Digiset Details Page
+    app.get('/digiset/details/:id', this.digisetDetailsView);
 };
