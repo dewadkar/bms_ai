407ea20cfcee77a0c9a6e84bdf4326c363e1cc53
SAMEER KHANDKE <sameer.khandke@gmail.com>
2019-08-20 10:52:38 +0530

    generate failure test data for generator

--- routes/digiset/digiset.js
+++ routes/digiset/digiset.js
@@ -2,40 +2,69 @@
 
 
 var location = '/routes/digiset/digiset';
+var status = require('../../lib/generator_status');
+
 
 module.exports = function (app) {
 
     /**
      * @function view
-     * @description Render the Digiset page 
+     * @description Render the Digiset page
      * @param request: HTTP Request
      * @param response: HTTP Response
      * @returns Render ejs page
      */
     this.view = function (request, response) {
         // var loc = location + 'view';
-        response.render('digiset/digiset', { title: "Digiset" });
+        response.render('digiset/digiset', {title: "Digiset"});
     };
+
 
     /**
      * @function digisetDetailsView
-     * @description Render the Digiset Details page 
+     * @description Render the Digiset Details page
      * @param request: HTTP Request
      * @param response: HTTP Response
      * @returns Render ejs page
      */
     this.digisetDetailsView = function (request, response) {
         // var loc = location + 'digisetDetailsView';
-        if(request.session.user_name){
-            response.render('digiset/digiset_details', { title: "Digiset Details", generator_id: request.params.id });
-        }else{
+        if (request.session.user_name) {
+            response.render('digiset/digiset_details', {title: "Digiset Details", generator_id: request.params.id});
+        } else {
             response.redirect('/');
         }
     };
+
+    this.generateData = function (request, response) {
+        status.status(function (error, data) {
+            if (error) {
+                response.send(error);
+            } else {
+                response.send(data);
+            }
+        })
+    };
+
+    this.getGeneratorDetails = function () {
+        var id = request.params.id;
+        status.getGeneratorDetails(id)
+            .then(function (data) {
+                response.send(data);
+            })
+            .catch(function (err) {
+                response.send(err);
+            })
+    };
+
 
     // Digiset Page
     app.get('/digiset', this.view);
 
     // Digiset Details Page
     app.get('/digiset/details/:id', this.digisetDetailsView);
+
+    app.get('/digiset/generate', this.generateData);
+    app.get('/digiset/generator/:id', this.getGeneratorDetails);
+
 };
--- routes/routes.js
+++ routes/routes.js
@@ -22,6 +22,5 @@ module.exports = function (app) {
     require('./user/logout')(app);
 
     // Generator Data
-    
 
 };
